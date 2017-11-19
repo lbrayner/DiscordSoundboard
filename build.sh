868bf84e7e40f32fe302f015d40ab4d72e9073ba
lbrayner <lbrayner@users.noreply.github.com>
2017-11-19 12:30:33 -0300

    Play only if it matches userid_to_join_channel
    When GUILD_VOICE_JOIN.
    
    build.sh
            Gradle instead of Maven. From POSIX to Bash.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 dir=`dirname "${0}"`
 cd "${dir}"
-mvn compile
+./gradlew classes
 zip -d build/libs/DiscordSoundboard-1.4.11.jar net/dirtydeeds/\*
-cd target/classes
-zip -g ../../build/libs/DiscordSoundboard-1.4.11.jar net/dirtydeeds/**/*
+cd build/classes/main
+zip -g ../../../build/libs/DiscordSoundboard-1.4.11.jar net/dirtydeeds/**/*
--- src/main/java/net/dirtydeeds/discordsoundboard/VoiceJoinSoundBoardListener.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/VoiceJoinSoundBoardListener.java
@@ -2,10 +2,11 @@ package net.dirtydeeds.discordsoundboard;
 
 import net.dv8tion.jda.hooks.ListenerAdapter;
 import net.dv8tion.jda.events.voice.VoiceJoinEvent;
-
 import net.dirtydeeds.discordsoundboard.service.SoundPlayerImpl;
-
 import net.dv8tion.jda.utils.SimpleLog;
+import net.dv8tion.jda.entities.User;
+
+import java.util.List;
 
 public class VoiceJoinSoundBoardListener extends ListenerAdapter {
 
@@ -25,8 +26,35 @@ public class VoiceJoinSoundBoardListener extends ListenerAdapter {
         final String botName = this.soundPlayerImpl.getProperty("bot_username");
         final String userName = event.getUser().getUsername();
         final String userNameToFollow = this.soundPlayerImpl.getProperty("username_to_join_channel");
+        final String userIdToFollow = this.soundPlayerImpl.getProperty("userid_to_join_channel");
+        final boolean userIdIsNull;
+        boolean foundUser = false;
+
+        List<User> users = event.getGuild().getUsersByName(userNameToFollow);
 
         LOG.info("Received a GUILD_VOICE_JOIN event.");
+
+        if(userIdToFollow == null)
+        {
+            LOG.debug("User ID is null.");
+            userIdIsNull = true;
+        }
+        else
+            userIdIsNull = false;
+
+        for(User user : users)
+        {
+            LOG.debug(String.format("User ID of %s is %s.",userNameToFollow,user.getId()));
+            if(!userIdIsNull)
+                if(user.getId().equals(userIdToFollow))
+                    foundUser = true;
+        }
+
+        if(!foundUser && !userIdIsNull)
+        {
+            LOG.info(String.format("Guild is other than %s's.",userNameToFollow));
+            return;
+        }
 
         if(fileName == null)
         {
